3d64ec20e7cdf002d91443c2eb5ea22cb24b2f04
Kevin Panesar <kevin_panesar@hotmail.com>
2021-09-27 17:57:48 -0700

    styled about us page

--- Components/AboutMePage/AboutMe.js
+++ Components/AboutMePage/AboutMe.js
@@ -1,54 +1,132 @@
-import React from 'react';
-import styled from 'styled-components'
-import Logo from '../../public/Media/logo.png';
+import { React, useState } from "react";
+import styled from "styled-components";
 import Image from "next/image";
+import { Header } from "../Header/Header";
+import Menu from "../sideMenu/Menu";
+import SideNavBar from "../sideMenu/SideNavBar";
 
 export const AboutMePage = () => {
-return(<Container>
-    <h1>About Us</h1>
-    <ImageDiv>
-        <Image src={Logo} />
-    </ImageDiv>
-    <h2>Our Mission</h2>
-    <AboutUs>
-        Providing you all anticipated sneaker and clothing release information in the metro vancouver area.
-    </AboutUs>
-    <h2>Our Story</h2>
-    <AboutUs>
-        We are two young guys that have a passion for collecting sneakers and collecting grailed items. We have many times struck out on sneaker drops, wether it be from  raffle draws or missing out on entering raffles due to the lack of information about rleeases in our area. Thats when we decided on creating a platform for all people like us that are interested in staying alert on when and where they can cop the latest hyped sneaker, clothes or merch drops.
-    </AboutUs>
-</Container>)
-}
+  const [open, setOpen] = useState(false);
+
+  return (
+    <Container>
+      <Header />
+      <div>
+        <SideNavBar open={open} setOpen={setOpen} />
+        <Menu open={open} setOpen={setOpen} />
+      </div>
+      <Content>
+        <LargeText>PURPOSE</LargeText>
+        <PurposeContainer>
+          <AboutUs>
+            Providing you all anticipated sneaker and clothing release
+            information in the metro vancouver area.
+          </AboutUs>
+        </PurposeContainer>
+        <ImageDiv>
+          <SneakerImg
+            src="/Media/aboutUsImage.jpg"
+            width="763px"
+            height="711px"
+          />
+          <ImageHighlight />
+        </ImageDiv>
+
+        <LargeText>ABOUT US</LargeText>
+        <AboutUsDescription>
+          We are two guys that have a passion for collecting sneakers and
+          collecting grailed items. We have many times struck out on sneaker
+          drops, wether it be from raffle draws or missing out on entering
+          raffles due to the lack of information about releases in our area.
+          Thats when we decided on creating a platform for all people like us
+          that are interested in staying alert on when and where they can cop
+          the latest hyped sneaker, clothes or merch drops.
+        </AboutUsDescription>
+      </Content>
+    </Container>
+  );
+};
 
 const Container = styled.div`
-width: 100%;
-display: flex;
-flex-direction: column;
-align-items: center;
-text-align: left;
-font-family: 'Inter', sans-serif;
-justify-content: space-evenly;
-margin-bottom: 80px;
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  text-align: left;
+  font-family: "Inter", sans-serif;
+  justify-content: space-evenly;
+  padding-bottom: 20px;
+  overflow: hidden;
 
-h1{
+  h1 {
     margin: 15px;
-}
+  }
 
-h2{
+  h2 {
     margin: 12px;
-}
-`
+  }
+`;
+
+const SneakerImg = styled(Image)`
+  z-index: 100;
+`;
+
+const LargeText = styled.p`
+  color: white;
+  font-size: 60px;
+  font-family: "Zen Tokyo Zoo", cursive;
+  margin-top: 35px;
+  margin-bottom: 10px;
+`;
+
+const ImageHighlight = styled.div`
+  position: absolute;
+  top: 20px;
+  right: 13px;
+  border: 3px solid #fdd017;
+  width: 100%;
+  height: 100%;
+  z-index: 50;
+`;
+
+const Content = styled.div`
+  background-color: black;
+  margin-top: 15px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+
+const PurposeContainer = styled.div`
+  width: 90%;
+  background-color: white;
+  padding: 5px;
+`;
 
 const ImageDiv = styled.div`
-    width: 85%;
-    height: 270px;
-    background-image: url("https://media.istockphoto.com/photos/moonrise-above-downtown-vancouver-picture-id1131455897?k=6&m=1131455897&s=612x612&w=0&h=rloje4jYWWHdoIWyOlhwSLevG-hDsm4Gd5dD2rxcBMY=");
-    border-radius: 10px;
-    background-size: cover;
-    display: flex;
-    justify-content: center;
-    align-content: center;
-`
+  background-size: cover;
+  display: flex;
+  justify-content: center;
+  align-content: center;
+  position: relative;
+  bottom: 10px;
+  left: 25px;
+  padding-left: 10px;
+  z-index: 100px;
+`;
 const AboutUs = styled.p`
-width: 80%;
-`
+  width: 80%;
+  font-family: "Share", "cursive";
+  font-size: 30px;
+  line-height: 1.3125;
+`;
+
+const AboutUsDescription = styled.p`
+  color: white;
+  font-family: "Share", "cursive";
+  width: 85%;
+  font-size: 24px;
+  line-height: 1.1;
+  padding-bottom: 20px;
+`;
